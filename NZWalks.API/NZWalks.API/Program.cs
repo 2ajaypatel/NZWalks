da75e274d7dbb865fb717e54958216bfa1de05da
ajay.patel <2ajaypatel@gmail.com>
2023-02-05 20:48:16 -0500

    modified files/

--- NZWalks.API/NZWalks.API/Program.cs
+++ NZWalks.API/NZWalks.API/Program.cs
@@ -1,3 +1,9 @@
+using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Data;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
+using Microsoft.OpenApi.Models;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +12,11 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<NZWalksDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("NZWalks"));
+});
 
 var app = builder.Build();
 
