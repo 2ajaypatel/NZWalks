e2b166da072458c693aa50a67ac70fcfee41755e
ajay.patel <2ajaypatel@gmail.com>
2023-02-13 14:57:48 -0500

    end of section 5.

---
+++ NZWalks.API/NZWalks.API/Repositories/IWalkRepository.cs
@@ -0,0 +1,15 @@
+﻿using NZWalks.API.Models.Domain;
+using System.Threading.Tasks;
+
+namespace NZWalks.API.Repositories
+{
+    public interface IWalkRepository
+    {
+        Task<IEnumerable<Walk>> GetAllAsync();
+        Task<Walk> GetWalkAsync(Guid id);
+        Task<Walk> AddAsync(Walk walk);
+        Task<Walk> UpdateAsync(Guid id, Walk walk);
+        Task<Walk> DeleteAsync(Guid id);
+        Task<IEnumerable<Walk>> GetWalkAsync(string name);
+    }
+} 
